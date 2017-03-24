2ca8a4013e24486016c2d7837947af790b947daf
arjunkapoorr <arjunkapoor817@gmail.com>
2017-03-24 15:27:00 +0530

    autocorrect implemented

--- mainViaCSV.py
+++ mainViaCSV.py
@@ -2,8 +2,45 @@ import csv
 import sys
 import requests
 import json
+from collections import Counter
+import re
+
 def result(pincode):
     print "Pincode for the location specified is " + pincode
+
+def autocorrect(input_word):
+    def words(text): return re.findall(r'\w+', text.lower())
+
+    WORDS = Counter(words(open('autocorrect.txt').read()))
+
+    def P(word, N=sum(WORDS.values())):
+        return WORDS[word] / N
+
+    def correction(word):
+        return max(candidates(word), key=P)
+
+    def candidates(word):
+        return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+    def known(words):
+        return set(w for w in words if w in WORDS)
+
+    def edits1(word):
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes = [L + R[1:] for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+        replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+        inserts = [L + c + R for L, R in splits for c in letters]
+        return set(deletes + transposes + replaces + inserts)
+
+    def edits2(word):
+        return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+    return correction(input_word)
+
+
+
 
 def callwithAPI(user_input):
     query = ""
@@ -31,11 +68,19 @@ def printinfo(Officename,Sub_distname,Districtname,Statename):
 def main():
     # declaring variable and converting user input to a list
     reader = csv.DictReader(open('Main.csv'))
-    user_input = raw_input("Enter the location of which you want the Pin Code")
-    user_input = user_input.split()
-    length = len(user_input)
+    user_inputraw = raw_input("Enter the location of which you want the Pin Code")
+    user_inputraw = user_inputraw.split()
+    length = len(user_inputraw)
+    user_input = []
     if length > 8:
         sys.exit('Enter a short query')
+    for keyword in user_inputraw:
+        corrected = autocorrect(keyword)
+        user_input.append(corrected)
+    # this is the auto corrected output
+    print user_input
+
+
     Sub_distname = ""
     Village = ""
     Officename = ""
